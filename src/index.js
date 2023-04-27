4cb73f1fb6a89f73b5699118a29292f2766a4d47
Imran Hossain <blur-imran@hotmail.com>
2023-04-27 22:39:33 +0600

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-// import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
 
